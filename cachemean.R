f8550f98e6d512241f2699a2aa9dbab35763aa70
Linda Slovick <LindaSlovick@gmail.com>
2014-05-22 11:09:38 -0700

    Added makeVector and cachemean

---
+++ cachemean.R
@@ -0,0 +1,14 @@
+## If mean already exists, get cached mean,
+## otherwise, calculate mean and cache it.
+
+cachemean <- function(x, ...) {
+        m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- mean(data, ...)
+        x$setmean(m)
+        m
+}
---
+++ makeVector.R
@@ -0,0 +1,16 @@
+## If the vector doesn't already exist, create it,
+## otherwise, return already-cached vector.
+
+makeVector <- function(x = numeric()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
+}
